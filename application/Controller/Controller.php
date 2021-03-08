ede5dcb7ca84d48a90d38876a02a18f93ff07b1c
James Zhao <james.k.zhao@gmail.com>
2021-03-08 11:46:33 -0800

    deleted unused functions

--- application/Controller/Controller.php
+++ application/Controller/Controller.php
@@ -6,18 +6,6 @@ class Controller{
     public static function show($user){
         echo Index::display($user);
     }
-    public static function showHead($page){
-        echo Header::display($page);
-    }
-    public static function showNav($user){
-        echo Nav::display($user);
-    }
-    public static function showFooter(){
-        echo Footer::display();
-    }
-    public static function showLoginModal(){
-        echo LoginModal::display();
-    }
     public static function getSessionUser(){
         return $_SESSION['current_user'];
     }
